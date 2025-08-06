94db6ce2e2e1049b39d144b2c145dd5251316275
Arnav Nanda <an255@duke.edu>
2025-08-06 12:40:15 -0400

    trying to fix astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
